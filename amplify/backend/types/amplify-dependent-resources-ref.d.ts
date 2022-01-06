d1c7af9417a9b1425a2f17411a283e9dd73ab210
Ojo Oluwasetemi <setemiojo@gmail.com>
2022-01-06 12:15:27 +0100

    📦 NEW: add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyappstartb829bd0a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import './App.css';
+import React from 'react';
 import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React with AWS Amplify
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>We now have Auth!</h1>
+			</header>
+			<AmplifySignOut />
+		</div>
+	);
 }
 
-export default App;
+export default withAuthenticator(App);
